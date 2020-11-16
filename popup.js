239768601750bd708d7893b35028235cd9e5c1c6
Erik P. Hansen <eph@erikphansen.com>
2020-11-15 19:01:50 -0800

    Look for one of two different session cookie names
    The name of the session cookie changed at some point. This PR updates
    the cookie-fetching logic to look for one of two different cookie
    named. I'm not sure if the cookie names have changed across the board or
    if the names have only changed for some users as part of the SSOe
    rollout.

--- popup.js
+++ popup.js
@@ -1,42 +1,48 @@
 const copyCookiesButton = document.querySelector('button');
-const API_SESSION_COOKIE_NAME = 'api_session';
-let VAGOV_SESSION_COOKIE_NAME;
+const API_COOKIE_NAME = 'api_session';
+let SESSION_COOKIE_NAME;
 
 const DOMAINS = {
   LOCAL: 'localhost',
-  STAGING: 'staging.va.gov',
   DEV: 'dev.va.gov',
+  STAGING: 'staging.va.gov',
   PROD: 'www.va.gov',
 };
 
 // The cookie domains (where to get the cookies from)
 const domainToCookieDomainMap = {
   [DOMAINS.LOCAL]: 'localhost',
-  [DOMAINS.STAGING]: '.va.gov',
   [DOMAINS.DEV]: '.va.gov',
+  [DOMAINS.STAGING]: '.va.gov',
   [DOMAINS.PROD]: '.va.gov',
 };
 // The domains of the API session cookie
 const apiSessionCookieDomains = {
   [DOMAINS.LOCAL]: 'localhost',
-  [DOMAINS.STAGING]: 'staging-api.va.gov',
   [DOMAINS.DEV]: 'dev-api.va.gov',
+  [DOMAINS.STAGING]: 'staging-api.va.gov',
   [DOMAINS.PROD]: 'api.va.gov',
 };
 // The domains of the VAGOV session cookie
 const vagovSessionCookieDomains = {
   [DOMAINS.LOCAL]: 'localhost',
-  [DOMAINS.STAGING]: '.va.gov',
   [DOMAINS.DEV]: '.va.gov',
+  [DOMAINS.STAGING]: '.va.gov',
   [DOMAINS.PROD]: '.va.gov',
 };
-// The names of the VAGOV session cookie; we look for a different cookie
-// depending on which domain/environment we are in
+// The possible names of the VAGOV session cookie. The cookie name will depend
+// on which environment we are running in. Also, for each environment, the
+// cookie name might vary based on the user's auth method. The cookie name for
+// each environment used to be stable, but the cookie names have changed. For
+// example, when testing locally the cookie name used to be 'vagov_session_dev'
+// but it is now 'vagov_saml_request_localhost'. I'm not sure if that change
+// affects all users or just some, so we are using an array of possible cookie
+// names and will use the first cookie that has a value.
 const domainToVAGOVSessionCookieMap = {
-  [DOMAINS.LOCAL]: 'vagov_session_dev',
-  [DOMAINS.STAGING]: 'vagov_session_staging',
-  [DOMAINS.DEV]: 'vagov_session_dev',
-  [DOMAINS.PROD]: 'vagov_session',
+  [DOMAINS.LOCAL]: ['vagov_session_dev', 'vagov_saml_request_localhost'],
+  [DOMAINS.DEV]: ['vagov_session_dev', 'vagov_saml_request_dev'],
+  [DOMAINS.STAGING]: ['vagov_session_staging', 'vagov_saml_request_staging'],
+  [DOMAINS.PROD]: ['vagov_session', 'vagov_saml_request_prod'],
 };
 
 function logIt(val) {
@@ -47,21 +53,21 @@ function logIt(val) {
  * Takes the two cookie values and creates a string that will serve as a value
  * for the 'Cookie' header in your REST client
  *
- * @param {string} cookie1 the value of the api_session cookie
- * @param {string} cookie2 the value of the vagov_session_dev cookie
+ * @param {string} cookie1 the value of the api cookie
+ * @param {string} cookie2 the value of the session cookie
  */
 function buildCookieHeaderValue(cookie1, cookie2) {
   if (
     !cookie1 ||
     !cookie2 ||
-    typeof cookie1 != 'string' ||
-    typeof cookie2 != 'string'
+    typeof cookie1 !== 'string' ||
+    typeof cookie2 !== 'string'
   ) {
     alert('Please pass two strings to `buildCookieHeaderValue`');
     window.close();
     return;
   }
-  return `${API_SESSION_COOKIE_NAME}=${cookie1}; ${VAGOV_SESSION_COOKIE_NAME}=${cookie2}`;
+  return `${API_COOKIE_NAME}=${cookie1}; ${SESSION_COOKIE_NAME}=${cookie2}`;
 }
 
 function copyToClipboard(string) {
@@ -89,27 +95,36 @@ function getDomainFromURL(url) {
 
 function getCookies(url) {
   const domain = getDomainFromURL(url);
-  VAGOV_SESSION_COOKIE_NAME = domainToVAGOVSessionCookieMap[domain];
+  const sessionCookieNames = domainToVAGOVSessionCookieMap[domain];
   chrome.cookies.getAll(
     { domain: domainToCookieDomainMap[domain] },
     cookies => {
       let apiSessionCookie = cookies.find(
         cookie =>
-          cookie.name === API_SESSION_COOKIE_NAME &&
+          cookie.name === API_COOKIE_NAME &&
           cookie.domain === apiSessionCookieDomains[domain],
       );
-      let vagovSessionCookie = cookies.find(
-        cookie =>
-          cookie.name === VAGOV_SESSION_COOKIE_NAME &&
-          cookie.domain === vagovSessionCookieDomains[domain],
-      );
+      let vagovSessionCookie;
+      // try getting the session cookie from all the potential cookie names
+      sessionCookieNames.forEach((cookieName) => {
+        if (!vagovSessionCookie) {
+          vagovSessionCookie = cookies.find(
+            (cookie) =>
+              cookie.name === cookieName &&
+              cookie.domain === vagovSessionCookieDomains[domain],
+          )
+          if (!!vagovSessionCookie) {
+            SESSION_COOKIE_NAME = cookieName
+          }
+        }
+      })
       if (!apiSessionCookie) {
-        alert(`Unable to find the '${API_SESSION_COOKIE_NAME}' cookie!`);
+        alert(`Unable to find the '${API_COOKIE_NAME}' cookie!`);
         window.close();
         return;
       }
       if (!vagovSessionCookie) {
-        alert(`Unable to find the '${VAGOV_SESSION_COOKIE_NAME}' cookie!`);
+        alert(`Unable to find any of the '${SESSION_COOKIE_NAME}' cookies!`);
         window.close();
         return;
       }
